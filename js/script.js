8459b2c07d2c09f6a892bc5a310e95dc6617307f
BenjaminPerrin <perrin.benjamin77@gmail.com>
2018-03-14 18:14:39 +0100

    fix navbar + responsive

--- js/script.js
+++ js/script.js
@@ -1,46 +1,63 @@
-// Bootstrap carousel
- 
- 
- 
-$('#carouselExample').on('slide.bs.carousel', function (e) {
- 
- 
-  var $e = $(e.relatedTarget);
-  var idx = $e.index();
-  var itemsPerSlide = 4;
-  var totalItems = $('.carousel-item').length;
- 
-  if (idx >= totalItems-(itemsPerSlide-1)) {
-      var it = itemsPerSlide - (totalItems - idx);
-      for (var i=0; i<it; i++) {
-          // append slides to end
-          if (e.direction=="left") {
-              $('.carousel-item').eq(i).appendTo('.carousel-inner');
-          }
-          else {
-              $('.carousel-item').eq(0).appendTo('.carousel-inner');
-          }
-      }
-  }
+
+$(document).ready(function () {
+    // executes when HTML-Document is loaded and DOM is ready
+    console.log("document is ready");
+
+    $(".nav2").clone().prependTo("#off-canvas");
+
+    $(function () {
+        $(document).trigger("enhance");
+    });
+
+    // document ready  
 });
- 
- 
+
+
+
+
+// Bootstrap carousel
+
+
+
+$('#carouselExample').on('slide.bs.carousel', function (e) {
+
+
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $('.carousel-item').length;
+
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i = 0; i < it; i++) {
+            // append slides to end
+            if (e.direction == "left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
+
+
 $('#carouselExample').carousel({
-              interval: 2000
-      });
- 
- 
-$(document).ready(function() {
-/* show lightbox when clicking a thumbnail */
-  $('a.thumb').click(function(event){
-    event.preventDefault();
-    var content = $('.modal-body');
-    content.empty();
-      var title = $(this).attr("title");
-      $('.modal-title').html(title);        
-      content.html($(this).html());
-      $(".modal-profile").modal({show:true});
-  });
- 
+    interval: 2000
+});
+
+
+$(document).ready(function () {
+    /* show lightbox when clicking a thumbnail */
+    $('a.thumb').click(function (event) {
+        event.preventDefault();
+        var content = $('.modal-body');
+        content.empty();
+        var title = $(this).attr("title");
+        $('.modal-title').html(title);
+        content.html($(this).html());
+        $(".modal-profile").modal({ show: true });
+    });
+
 });
   //Chatbox
