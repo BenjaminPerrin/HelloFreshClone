d5a20565571b2310a51f1473b3c7646f184734c1
Bastien Champion <champion.bas@gmail.com>
2018-03-15 12:13:21 +0100

    premerge

---
+++ js/hellofresh.js
@@ -0,0 +1,69 @@
+
+// Plats carousel
+
+$('#carouselExample').on('slide.bs.carousel', function (e) {
+
+  
+  var $e = $(e.relatedTarget);
+  var idx = $e.index();
+  var itemsPerSlide = 4;
+  var totalItems = $('.carousel-item').length;
+  
+  if (idx >= totalItems-(itemsPerSlide-1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i=0; i<it; i++) {
+          // append slides to end
+          if (e.direction=="left") {
+              $('.carousel-item').eq(i).appendTo('.carousel-inner');
+          }
+          else {
+              $('.carousel-item').eq(0).appendTo('.carousel-inner');
+          }
+      }
+  }
+});
+
+
+$('#carouselExample').carousel({ 
+              interval: 2000
+      });
+
+
+$(document).ready(function() {
+/* show lightbox when clicking a thumbnail */
+  $('a.thumb').click(function(event){
+    event.preventDefault();
+    var content = $('.modal-body');
+    content.empty();
+      var title = $(this).attr("title");
+      $('.modal-title').html(title);        
+      content.html($(this).html());
+      $(".modal-profile").modal({show:true});
+  });
+
+});
+
+  //Chatbox
+  
+  (function($) {
+    $(document).ready(function() {
+        var $chatbox = $('.chatbox'),
+            $chatboxTitle = $('.chatbox__title'),
+            $chatboxTitleClose = $('.chatbox__title__close'),
+            $chatboxCredentials = $('.chatbox__credentials');
+        $chatboxTitle.on('click', function() {
+            $chatbox.toggleClass('chatbox--tray');
+        });
+        $chatboxTitleClose.on('click', function(e) {
+            e.stopPropagation();
+            $chatbox.addClass('chatbox--closed');
+        });
+        $chatbox.on('transitionend', function() {
+            if ($chatbox.hasClass('chatbox--closed')) $chatbox.remove();
+        });
+        $chatboxCredentials.on('submit', function(e) {
+            e.preventDefault();
+            $chatbox.removeClass('chatbox--empty');
+        });
+    });
+})(jQuery);
